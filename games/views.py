1c749aada00fb8311ba8dfe9a18f3fa6d3e903ab
Dom-888 <spinelladomenico88@gmail.com>
2020-08-05 18:17:53 +0000

    Add games page

--- games/views.py
+++ games/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Game
 
-# Create your views here.
+
+# Show all the games on sale on the site
+def all_games(request):
+    games = Game.objects.all()
+
+    context = {
+        'games': games,
+    }
+
+    return render(request, 'games/games.html', context)
