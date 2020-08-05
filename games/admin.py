c5b99d8f24e0396c775d0a66c7c12d1d012b755d
Dom-888 <spinelladomenico88@gmail.com>
2020-08-05 17:44:21 +0000

    Register game models in admin.py

--- games/admin.py
+++ games/admin.py
@@ -3,6 +3,30 @@ from .models import Game, Category, Included
 
 # Register your models here.
 
-admin.site.register(Game)
-admin.site.register(Category)
-admin.site.register(Included)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+class IncludedAdmin(admin.ModelAdmin):
+    list_display = (
+        'game',
+        'quantity',
+        'item'
+    )
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Included, IncludedAdmin)
