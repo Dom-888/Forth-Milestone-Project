85f4cf1282bd812656e3c4c30e5968fbaf8a222d
Dom-888 <spinelladomenico88@gmail.com>
2020-08-06 10:16:00 +0000

    Add a draft of the game details page

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_games, name='games')
+    path('', views.all_games, name='games'),
+    path('<game_pk>', views.game_details, name='game_details'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Game
+from django.shortcuts import render, get_object_or_404
+from .models import Game, Included
 
 
 # Show all the games on sale on the site
@@ -11,3 +11,19 @@ def all_games(request):
     }
 
     return render(request, 'games/games.html', context)
+
+
+# Show the details of a specific game
+def game_details(request, game_pk):
+
+    game = get_object_or_404(Game, pk=game_pk)
+
+    # I need a way to select only the icluded instance where included.game = game.pk
+    included = Included.objects.all()
+
+    context = {
+        'game': game,
+        'included': included
+    }
+
+    return render(request, 'games/game_details.html', context)
