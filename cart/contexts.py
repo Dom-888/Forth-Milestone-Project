00844534d6b3860801b540acb7943d177694407c
Dom-888 <spinelladomenico88@gmail.com>
2020-08-07 17:31:25 +0000

    Add the view to add items to cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from games.models import Game
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        game = get_object_or_404(Game, pk=item_id)
+        total += quantity * game.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'game': game,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
