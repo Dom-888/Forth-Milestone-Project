3b238ef9023e29f7948b764753f3b06916a1ab0d
Dom-888 <spinelladomenico88@gmail.com>
2020-08-07 11:32:46 +0000

    Add functionality to the searchbar

--- games/views.py
+++ games/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Game, Included
 
 
-# Show all the games on sale on the site
+# Show the games on sale on the site
 def all_games(request):
     games = Game.objects.all()
+    included = Included.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter one or more words to search for")
+                return redirect(reverse('games'))
+
+            # Apply the search query to the Included model
+            i_queries = Q(item__icontains=query)
+            items = included.filter(i_queries)
+            for i in items:
+                print(i.game)
+
+
+            # Apply the search query to the Games 
+            g_queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(name__icontains=i.game) 
+            games = games.filter(g_queries)
 
     context = {
         'games': games,
+        'search_term': query,
     }
 
     return render(request, 'games/games.html', context)
