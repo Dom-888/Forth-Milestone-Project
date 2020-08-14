ca2e20c183f50211a03d75bcb6a06013312f2a21
Dom-888 <spinelladomenico88@gmail.com>
2020-08-14 13:47:09 +0000

    Add error toast

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from games.models import Game
 
@@ -37,16 +37,22 @@ def remove_cart_item(request, item_id):
 
 # Change item quantity in the cart
 def update_cart_item_quantity(request, item_id):
-    game = get_object_or_404(Game, pk=item_id)
-    quantity = int(request.POST.get("quantity"))
-    cart = request.session.get("cart", {})
 
-    if quantity > 0:
-        cart[item_id] = quantity
-        messages.success(request, f'Quantity for {game.name} updated') # Toast message
-    else:
-        cart.pop(item_id)
-        messages.success(request, f'{game.name} removed from cart') # Toast message
+    try:
+        game = get_object_or_404(Game, pk=item_id)
+        quantity = int(request.POST.get("quantity"))
+        cart = request.session.get("cart", {})
 
-    request.session["cart"] = cart
-    return redirect(reverse("view_cart"))
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request, f'Quantity for {game.name} updated') # Toast message
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'{game.name} removed from cart') # Toast message
+
+        request.session["cart"] = cart
+        return redirect(reverse("view_cart"))
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
