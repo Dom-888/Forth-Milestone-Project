435a756fcf89d80325b39ed5ea7fff3d639fa190
Dom-888 <spinelladomenico88@gmail.com>
2020-08-14 11:24:14 +0000

    Add toast message for when a new item is added to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from games.models import Game
 
 # Show the cart page
 def view_cart(request):
@@ -8,6 +10,8 @@ def view_cart(request):
 # Add the specified quantity of the selected game to the cart
 def add_to_cart(request, item_id):
 
+    game = Game.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -16,6 +20,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'{game.name} added to cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,5 @@
 // Replace all elements that have a data-feather attribute with a Feather icon
 feather.replace();
+
+// Show toast messages
+$('.toast').toast('show');
