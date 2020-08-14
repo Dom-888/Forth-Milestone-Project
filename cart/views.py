46311399c488f6ad1f727aca6d9556acdcdb94bd
Dom-888 <spinelladomenico88@gmail.com>
2020-08-14 13:31:43 +0000

    Add a toast message for when a product is updated or removed from the cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from games.models import Game
 
@@ -10,38 +10,43 @@ def view_cart(request):
 # Add the specified quantity of the selected game to the cart
 def add_to_cart(request, item_id):
 
-    game = Game.objects.get(pk=item_id)
-
+    game = get_object_or_404(Game, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'{game.name} added to cart') # Toast message
     else:
         cart[item_id] = quantity
-        messages.success(request, f'{game.name} added to cart')
+        messages.success(request, f'{game.name} added to cart') # Toast message
 
     request.session['cart'] = cart
     return redirect(redirect_url)
 
 # Remove the item from the cart
 def remove_cart_item(request, item_id):
+    game = get_object_or_404(Game, pk=item_id)
     cart = request.session.get("cart", {})
     cart.pop(item_id)
+    messages.success(request, f'{game.name} removed from cart') # Toast message
+
     request.session["cart"] = cart
-    # Message: "Item removed from cart"
     return redirect(reverse("view_cart"))
 
 # Change item quantity in the cart
 def update_cart_item_quantity(request, item_id):
+    game = get_object_or_404(Game, pk=item_id)
     quantity = int(request.POST.get("quantity"))
     cart = request.session.get("cart", {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Quantity for {game.name} updated') # Toast message
     else:
         cart.pop(item_id)
+        messages.success(request, f'{game.name} removed from cart') # Toast message
 
     request.session["cart"] = cart
     return redirect(reverse("view_cart"))
