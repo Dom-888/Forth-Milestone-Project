b77fa6b5a6256e1a90d35df7f6ef20593f4ac607
Dom-888 <spinelladomenico88@gmail.com>
2020-08-25 12:46:54 +0000

    Return to top button

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,21 @@ feather.replace();
 // Show toast messages
 $('.toast').toast('show');
 
+// toTopButton
+
+// When the user scrolls down 20px from the top of the document, show the return-to-top button
+window.onscroll = function() { scrollFunction(); };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $("#toTopButton").show();
+    } else {
+        $("#toTopButton").hide();
+    }
+}
+
+//  When the user clicks on the return-to-top button, scroll to the top of the document
+function returnToTop() {
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
+    console.log("porcoddio")
+}
