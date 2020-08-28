f778773b4cbe732b36dc7e4b56d43553cbcf052d
Dom-888 <spinelladomenico88@gmail.com>
2020-08-28 06:24:22 +0000

    Remove secret key and set debug

--- the_yellow_dice/settings.py
+++ the_yellow_dice/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=em-**6n3uqoccel_#znw89!)v2z=v#*tv)rcsf!uv96*m1hg-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-yellow-dice.herokuapp.com', 'localhost']
 
