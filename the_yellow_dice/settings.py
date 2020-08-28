d17cec3f0ca4c5893531c3fc02ab456807ff29f4
Dom-888 <spinelladomenico88@gmail.com>
2020-08-28 05:59:58 +0000

    Link production database to repository

--- the_yellow_dice/settings.py
+++ the_yellow_dice/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '=em-**6n3uqoccel_#znw89!)v2z=v#*tv)rcsf!uv96*m1hg-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-yellow-dice.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -128,12 +128,18 @@ WSGI_APPLICATION = 'the_yellow_dice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ: # Production database
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: # Development database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
